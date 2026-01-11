cc9b226fdebf562b0f1bb1234a6809970bb654de
ulasserbetcioglu <46876065+ulasserbetcioglu@users.noreply.github.com>
2026-01-12 00:30:28 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      // Bu satır React Native kodlarını web'de çalışır hale getirir
+      'react-native': 'react-native-web',
     },
+  },
+  // Global nesne tanımlaması (bazen ikon kütüphaneleri için gerekir)
+  define: {
+    global: 'window',
   },
 });
