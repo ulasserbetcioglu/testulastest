d1c89530f18884e5cc6b2c5107bdbfdd4fd997c7
ulasserbetcioglu <46876065+ulasserbetcioglu@users.noreply.github.com>
2026-02-09 16:16:50 +0300

    Updated Sidebar.tsx

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -82,6 +82,7 @@ const menuCategories = [
     items: [
       { name: 'Takvim Gönder', path: '/aylik-takvim-eposta', icon: <Mail size={18} /> },
       { name: 'Ekipman Paz.', path: '/ekipman-pazarlama', icon: <Package size={18} /> },
+      { name: 'Ekipman Yönetimi', path: '/ekipman-yonetimi', icon: <Package size={18} /> },
       { name: 'Hizmet Paz.', path: '/hizmet-pazarlama', icon: <MessageSquare size={18} /> }, // YENİ
        { name: 'Hizmet Yönetimi', path: '/hizmet-yonetimi', icon: <MessageSquare size={18} /> }, // YENİ
       { name: 'Eğitim Sunum.', path: '/egitim-sunumlari', icon: <Presentation size={18} /> }, // YENİ
